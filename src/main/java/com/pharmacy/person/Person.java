a101ff990e724682ab98e4aa0906f2ff3d34146f
YefersonMauricioDiazChogo21 <yefersondiaz2001@gmail.com>
2024-07-18 12:53:18 -0500

    feat: :sparkles: add person

--- src/main/java/com/pharmacy/person/Person.java
+++ src/main/java/com/pharmacy/person/Person.java
@@ -6,4 +6,49 @@ public class Person {
     private String lastname;
     private int age;
     private int birthdate;
+
+    public Person() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(int birthdate) {
+        this.birthdate = birthdate;
+    }
+    
+    
 }
